a6d4a1f36119560d527e23688e7180c1f9926eda
Phill Legnitto <palegnitto@gmail.com>
2022-10-18 20:53:51 -0700

    reanamed file to match and added docstrings

--- day3.js
+++ day3.js
@@ -1,3 +1,32 @@
+/**
+Considering only the first bit of each number, there are five 0 bits and seven
+1 bits. Since the most common bit is 1, the first bit of the gamma rate is 1.
+
+The most common second bit of the numbers in the diagnostic report is 0, so the
+ second bit of the gamma rate is 0.
+
+The most common value of the third, fourth, and fifth bits are 1, 1, and 0,
+respectively, and so the final three bits of the gamma rate are 110.
+
+So, the gamma rate is the binary number 10110, or 22 in decimal.
+
+The epsilon rate is calculated in a similar way; rather than use the most common
+ bit, the least common bit from each position is used. So, the epsilon rate is
+ 01001, or 9 in decimal. Multiplying the gamma rate (22) by the epsilon rate (9)
+  produces the power consumption, 198.
+
+Use the binary numbers in your diagnostic report to calculate the gamma rate and
+epsilon rate, then multiply them together. What is the power consumption of the
+submarine? (Be sure to represent your answer in decimal, not binary.)
+
+ */
+
+
+// Gamma is most common bit
+// Epsilon is least common bits
+//  power = gamma X epsiolon as decimals
+
+
 
 let codeMatrix =[[011110111101],
 [110010010001],
@@ -1000,6 +1029,10 @@ let codeMatrix =[[011110111101],
 [100111100101],
 [111111000010]]
 
+
+/**
+ *  Takes a binary number as a string and returns it's decimal number.
+ */
 function convertBToD(binaryNum){
   let count = 0;
   let arrOfDigits = binaryNum.split("");
@@ -1013,6 +1046,11 @@ function convertBToD(binaryNum){
   return count
 }
 
+
+/**
+ * Takes an array of arrays of binary numbers and returns an array with one
+ * Gamma and one Epsilon in string format;
+ */
 function decode(code){
   let count1 = 0;
   let count0 = 0;
@@ -1022,6 +1060,8 @@ function decode(code){
   for (let i = 0; i < code[0].length; i++){
     for(let arr of code){
       arr[i] === 1 ? count1++ : count0++;
+      console.log('arr',arr,'code',code);
+      console.log('i',i,'arr i', arr[i]);
     }
     if (count1 >= count0) {
       gamma = gamma + "1";
@@ -1036,7 +1076,7 @@ function decode(code){
   return [gamma,epsilon];
 }
 
-
+// calculates the power
 function calcPower(matrix) {
 
   let [gamma,epsilon] = decode(matrix);
